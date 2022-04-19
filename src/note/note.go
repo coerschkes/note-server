c799f6cd9a71d375516583595e7e400a95e432fc
Christian Oerschkes <christian.oerschkes@hotmail.de>
2022-04-20 00:25:38 +0200

    Implement the base server

---
+++ src/note/note.go
@@ -0,0 +1,14 @@
+package note
+
+type Note struct {
+	ID      int64  `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+type NoteRepository interface {
+	GetAll() ([]Note, error)
+	GetById(id int64) (Note, error)
+	Add(note Note) error
+	DeleteById(id int64) error
+}

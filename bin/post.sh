d4febe381d969aa3a058ec876f48fe30047ff5cb
Christian Oerschkes <christian.oerschkes@hotmail.de>
2022-04-20 15:14:09 +0200

    Improve scripts

--- bin/post.sh
+++ bin/post.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -v http://localhost:8080/notes --include --header "Content-Type: application/json" --data '{"id": 3, "title": "Test3", "content": "this is the third test"}'
+curl -v http://localhost:8080/notes --include \
+ --header "Content-Type: application/json" \
+ --data '{"id": 3, "title": "Test3", "content": "this is the third test"}'

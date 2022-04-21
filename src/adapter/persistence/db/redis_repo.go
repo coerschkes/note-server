4728c29d664dab0d32bf6a7e8b5eaf2666d58a29
Christian Oerschkes <christian.oerschkes@hotmail.de>
2022-04-21 14:15:04 +0200

    Implement a local database repository using redis

---
+++ src/adapter/persistence/db/redis_repo.go
@@ -0,0 +1,73 @@
+package db
+
+import (
+	"co/note-server/src/domain/model"
+	"errors"
+
+	"github.com/go-redis/redis"
+)
+
+const databaseAddress = "localhost:6379"
+
+type RedisRepository struct {
+	rdb *redis.Client
+}
+
+func MakeRedisRepository() RedisRepository {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     databaseAddress,
+		Password: "",
+		DB:       0,
+	})
+	return RedisRepository{rdb: rdb}
+}
+
+func (r RedisRepository) GetAll() ([]model.Note, error) {
+	if keys, err := r.rdb.Keys("*").Result(); err != nil {
+		return nil, err
+	} else {
+		notes := []model.Note{}
+		for _, key := range keys {
+			if note, err := r.GetById(key); err != nil {
+				return nil, err
+			} else {
+				notes = append(notes, note)
+			}
+		}
+		return notes, nil
+	}
+}
+
+func (r RedisRepository) GetById(id string) (model.Note, error) {
+	if note, err := r.rdb.Get(id).Result(); err == redis.Nil {
+		return model.MakeInvalidNote(), errors.New("Note with id '" + id + "' does not exist")
+	} else if err != nil {
+		return model.MakeInvalidNote(), err
+	} else {
+		return model.FromJson(note)
+	}
+}
+
+func (r RedisRepository) Add(note model.Note) error {
+	if jsn, err := note.ToJson(); err != nil {
+		return err
+	} else {
+		if r.noteExists(note.ID) {
+			return errors.New("Note with id '" + note.ID + "' already exists.")
+		}
+		return r.rdb.Set(note.ID, jsn, 0).Err()
+	}
+}
+
+func (r RedisRepository) DeleteById(id string) error {
+	if r.noteExists(id) {
+		return r.rdb.Del(id).Err()
+	} else {
+		return errors.New("Note with id '" + id + "' does not exist.")
+	}
+}
+
+func (r RedisRepository) noteExists(id string) bool {
+	n, _ := r.GetById(id)
+	return n.ID != model.MakeInvalidNote().ID
+}
--- src/main.go
+++ src/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"co/note-server/src/adapter/api"
-	"co/note-server/src/adapter/persistence/ram"
+	"co/note-server/src/adapter/persistence/db"
 )
 
 func main() {
-	var server api.NoteServer = api.NewNoteController(ram.MakeInMemoryNoteRepository())
+	var server api.NoteServer = api.NewNoteController(db.MakeRedisRepository())
 
 	server.InitServer()
 }
